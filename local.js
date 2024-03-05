729aa1b849b39d4f88869b58ba2c711e6f46ec43
masud <masudranaacb@gmail.com>
2024-03-05 11:29:54 +0600

    get item and set item

--- local.js
+++ local.js
@@ -1,10 +1,36 @@
-const setItemLocal=()=>{
-    localStorage.setItem("name","masud")
-    const person={
-        name:"ali",
-        age:2,
-        color:"white"
+const addToCard = () => {
+    const productName = document.getElementById("product_Name")
+    const productQuntity = document.getElementById("product_quntity")
+    const productNameValue = productName.value
+    const productQuntityValue = productQuntity.value
+    productName.value = ""
+    productQuntity.value = ""
+    displayAddedProduct(productNameValue, productQuntityValue)
+    saveStoredCard(productNameValue,productQuntityValue)
+}
+
+const displayAddedProduct = (product, quntity) => {
+    const productConatiner = document.getElementById("add_product_container")
+    const li = document.createElement("li")
+    li.innerText = `${product} : ${quntity}`
+    productConatiner.appendChild(li)
+
+}
+const getLoacalStorage = () => {
+    let card={}
+    const getCard=localStorage.getItem("card")
+    if(getCard){
+           card=JSON.parse(getCard) 
     }
-    const personJson=JSON.stringify(person)
-    localStorage.setItem("person",personJson)
+    return card
+}
+
+
+const saveStoredCard=(product,quntity)=>{
+    const card=getLoacalStorage()
+    card[product]=quntity
+
+    const cardStringify=JSON.stringify(card)
+    console.log(card)
+    localStorage.setItem("card",cardStringify)
 }
